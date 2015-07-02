5f1531cdc224caed82ec8c637292bff83bece8ae
USOLVED <git@usolved.net>
2015-07-02 14:45:26 +0200

    Initial Release

---
+++ README.md
@@ -0,0 +1,201 @@
+# check_usolved_vsphere_alarms
+
+## Overview
+
+This Perl Nagios/Icinga plugin retrieves all alarms from VMWare vSphere.
+You can filter the alarms with various options for ex- and including items.
+
+If you're already using alarm rules in vSphere this plugin may help you to have your monitoring in one place instead of two.
+
+## Authors
+
+Ricardo Klement (www.usolved.net)
+
+## Installation
+
+Just copy the file check_usolved_vsphere_alarms.pl into your Nagios plugin directory.
+For example into the path /usr/local/nagios/libexec/
+
+Add execution permission for the nagios user on check_usolved_vsphere_alarms.php.
+If you have at least Perl 5 and the VMWare Perl SDK  installed this plugin should run out-of-the-box.
+
+If you get errors while executing the plugin install the missing modules.
+
+###Install default Perl modules
+
+With yum package manager (RedHat, CentOS, ...)
+<pre><code>
+yum install perl-Getopt-Long-Descriptive
+yum install perl-File-Spec
+</code></pre>
+
+Or with CPAN
+<pre><code>
+cpan install Getopt::Long
+cpan install File::Spec
+</code></pre>
+
+
+###Install VMWare Perl SDK
+
+This plugin needs the VMWare Perl SDK for connecting to the vSphere or ESX servers. If you are using other plugins like check_vmware_esx.pl or check_esx3.pl you may already have this SDK installed. If you don't have it, please fallow these steps to install it.
+
+1. Go go [VMWare](https://my.vmware.com/web/vmware/downloads) and search for Perl SDK
+2. Download the proper Perl SDK for your vSphere release (you need to register at VMWare to download the SDK)
+3. Upload the tar.gz to your Nagios/Icinga server and install it
+
+<pre><code>
+tar -xzf VMware-vSphere-Perl-SDK-5.5.0-1384587.x86_64.tar.gz
+cd vmware-vsphere-cli-distrib
+./vmware-install.pl
+</code></pre>
+
+
+## Usage
+
+### Test on command line
+If you are in the Nagios plugin directory execute this command:
+
+<pre><code>
+./check_usolved_vsphere_alarms.pl -H 172.0.1.1 -U username -P password
+</code></pre>
+
+The output could look like this:
+
+<pre><code>
+Critical - 10 alarms found. View extended output for more information.
+Critical - Network-Uplink-Redundancy lost on ESX1 (DATACENTER1: HostSystem)
+Warning - CPU-Usage of Virtual Machine on SERVER3 (DATACENTER2: VirtualMachine)
+Critical - CPU-Usage of Virtual Machine on SERVER1 (DATACENTER2: VirtualMachine)
+Critical - Data storage disk on DATASTORE1 (DATACENTER1: Datastore)
+Critical - Data storage disk on DATASTORE2 (DATACENTER1: Datastore)
+Warning - Data storage disk on DATASTORE3 (DATACENTER1: Datastore)
+Warning - Data storage disk on DATASTORE4 (DATACENTER1: Datastore)
+Warning - Data storage disk on DATASTORE5 (DATACENTER1: Datastore)
+Critical - SimpliVity OmniCube Available Physical Capacity 10 Percent or Less on ESX3 (DATACENTER1: HostSystem)
+Critical - SimpliVity Datacenter Available Physical Capacity 10 Percent or Less on SERVER2 (DATACENTER1: Datacenter)
+</code></pre>
+
+Here's an example with some filters:
+
+<pre><code>
+./check_usolved_vsphere_alarms.pl -H 172.0.1.1 -U username -P password -C i:type=ds,i:status=critical
+</code></pre>
+
+The output could look like this:
+
+<pre><code>
+Critical - 2 alarms found. View extended output for more information.
+Critical - Data storage disk on DATASTORE1 (DATACENTER1: Datastore)
+Critical - Data storage disk on DATASTORE2 (DATACENTER1: Datastore)
+</code></pre>
+
+Here are all arguments that can be used with this plugin:
+
+<pre><code>
+-H, --hostname=HOST
+    Name or IP address of host to check
+-U, --username=USERNAME
+    Local or domain user for vSphere. If you use windows login make sure to escape the backslash like domainname\\\\username
+-P, --password=PASSWORD
+    Password for the given username
+-C, --check=CHECK
+    Specify what you want to check. By default the plugin checks all alarms.
+
+    Syntax:
+    &lt;MODE&gt;:&lt;SELECT&gt;=&lt;SUBSELECT&gt;
+
+    MODE:
+    	i = include check
+    	e = exclude check
+
+    SELECT:
+    	type = just check vms, ds or esx hosts alarms
+
+    		SUBSELECT:
+    		vm  = virtual machine
+    		ds  = datastore
+    		esx = host system
+
+    	object = to check a specific object name like a vm name
+
+    		SUBSELECT:
+    		name of the vm, ds or esx host. wildcard is allowed like *vmname*
+
+    	status = just show either warnings or criticals
+
+    		SUBSELECT:
+    		warning  = alarms marked as warnings
+    		critical = alarms marked as criticals
+
+    	datacenter = to filter alarms for a specific datacenter
+
+    		SUBSELECT:
+    		name of your datacenter
+
+
+    &lt;i|e&gt;:type:&lt;vm|ds|esx&gt;
+    &lt;i|e&gt;:object:&lt;name of your vm, ds or esx&gt;
+    &lt;i|e&gt;:status:&lt;warning|critical&gt;
+    &lt;i|e&gt;:datacenter:&lt;name of your datacenter&gt;
+
+    Examples:
+    1. just show alarms from esx hosts
+    ./check_usolved_vsphere_alarms.pl -H 172.0.1.1 -U username -P password -C i:type=esx
+
+    2. check all datastores with critical alarms but don't show datastores with name dsname03
+    ./check_usolved_vsphere_alarms.pl -H 172.0.1.1 -U username -P password -C i:type=ds,i:status=critical,e:object=dsname03
+
+    Remember that the include has a higher priority than the exclude.
+</code></pre>
+
+### Install in Nagios/Icinga
+
+Edit your **commands.cfg** and add the following.
+
+Example for basic check (with host macro for username and password):
+
+<pre><code>
+define command {
+    command_name    check_usolved_vsphere_alarms
+    command_line    $USER1$/check_usolved_vsphere_alarms.pl -H $HOSTADDRESS$ -U $_HOSTUSER$ -P $_HOSTPASSWORD$
+}
+</code></pre>
+
+Example for using a filter:
+
+<pre><code>
+define command {
+    command_name    check_usolved_vsphere_alarms
+    command_line    $USER1$/check_usolved_vsphere_alarms.pl -H $HOSTADDRESS$ -U $_HOSTUSER$ -P $_HOSTPASSWORD$ -C $ARG1$
+}
+</code></pre>
+
+Edit your **services.cfg** and add the following.
+
+Example for basic check with all alarms:
+
+<pre><code>
+define service{
+	host_name				Test-Server
+	service_description		vSphere-Alarms
+	use						generic-service
+	check_command			check_usolved_vsphere_alarms
+}
+</code></pre>
+
+Example for excluding warnings:
+
+<pre><code>
+define service{
+	host_name				Test-Server
+	service_description		vSphere-Alarms
+	use						generic-service
+	check_command			check_usolved_vsphere_alarms!i:status=warning
+}
+</code></pre>
+
+## Whats new
+
+1.0 Initial release
+
---
+++ check_usolved_vsphere_alarms.pl
@@ -0,0 +1,433 @@
+#!/usr/bin/perl -w
+#
+#This Perl Nagios/Icinga plugin retrieves all alarms from VMWare vSphere.
+#You can filter the alarms with various options for ex- and including items.
+#If you're already using alarm rules in vSphere this plugin may help you to have 
+#your monitoring in one place instead of two.
+#
+#The pluging needs the VMWare Perl SDK to be installed on your monitoring server.
+#This plugin has been tested with the SDK v5.0 and v5.5.
+#
+#
+#Copyright (c) 2015 www.usolved.net 
+#Published under https://github.com/usolved/check_usolved_vsphere_alarms
+#
+#
+#This program is free software; you can redistribute it and/or
+#modify it under the terms of the GNU General Public License
+#as published by the Free Software Foundation; either version 2
+#of the License, or (at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty
+#of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+#---------------------------------------------------------------------------------------
+#
+#v1.0
+#Initial release
+#
+
+
+
+use strict;
+use warnings;
+
+#---------------------------------------------------------------------------------------
+# Load modules
+
+use VMware::VIRuntime;	#To access the VMWare SDK
+use Getopt::Long;		#Parse the parameters
+use File::Spec;			#For splitting the path to get the filename
+
+
+#---------------------------------------------------------------------------------------
+# Define variables
+
+my $output_nagios 			= "";
+my $output_nagios_extended	= "";
+my $output_return_code 		= 0;
+
+my $count_warning 			= 0;
+my $count_critical 			= 0;
+
+my ($option_host, $option_username, $option_password, $option_check);
+my %checks;
+
+
+my ($volume, $directory, $file) = File::Spec->splitpath(__FILE__);
+my $scriptname = $file;
+
+
+#---------------------------------------------------------------------------------------
+# Define functions
+
+
+#Print out the help information
+sub output_usage
+{
+   print <<EOT;
+-H, --hostname=HOST
+    Name or IP address of host to check
+-U, --username=USERNAME
+    Local or domain user for vSphere. If you use windows login make sure to escape the backslash like domainname\\\\username
+-P, --password=PASSWORD
+    Password for the given username
+-C, --check=CHECK
+    Specify what you want to check. By default the plugin checks all alarms.
+
+    Syntax:
+    <MODE>:<SELECT>=<SUBSELECT>
+
+    MODE:
+    	i = include check
+    	e = exclude check
+
+    SELECT:
+    	type = just check vms, ds or esx hosts alarms
+
+    		SUBSELECT:
+    		vm  = virtual machine
+    		ds  = datastore
+    		esx = host system
+
+    	object = to check a specific object name like a vm name
+
+    		SUBSELECT:
+    		name of the vm, ds or esx host. wildcard is allowed like *vmname*
+
+    	status = just show either warnings or criticals
+
+    		SUBSELECT:
+    		warning  = alarms marked as warnings
+    		critical = alarms marked as criticals
+
+    	datacenter = to filter alarms for a specific datacenter
+
+    		SUBSELECT:
+    		name of your datacenter
+
+
+    <i|e>:type:<vm|ds|esx>
+    <i|e>:object:<name of your vm, ds or esx>
+    <i|e>:status:<warning|critical>
+    <i|e>:datacenter:<name of your datacenter>
+
+    Examples:
+    1. just show alarms from esx hosts
+    ./$scriptname -H 172.0.1.1 -U username -P password -C i:type=esx
+
+    2. check all datastores with critical alarms but don't show datastores with name dsname03
+    ./$scriptname -H 172.0.1.1 -U username -P password -C i:type=ds,i:status=critical,e:object=dsname03
+
+    Remember that the include has a higher priority than the exclude.
+
+EOT
+	exit 3;
+}
+
+
+#get all the parameters and to a little error handling
+sub get_options
+{
+	if(@ARGV < 1)
+	{
+		output_usage();
+	}
+
+    Getopt::Long::Configure ("bundling");
+    GetOptions(
+        "H|hostname=s"     => \$option_host,
+        "U|username=s"     => \$option_username,
+        "P|password=s"     => \$option_password,
+        "C|check=s"        => \$option_check
+	);
+
+    if(!defined($option_host) || !defined($option_username) || !defined($option_password))
+ 	{ 
+ 		print "Error - hostname, username and password are required.\n";
+ 		output_usage();
+ 	}
+
+}
+
+#connect to the vsphere server with user and password
+sub connect_vsphere
+{
+	Util::connect("https://" . $option_host . "/sdk/webService", $option_username, $option_password);
+}
+
+#when parameter -C is given check for all the include/exclude rules
+sub check_rules
+{
+	my($e_or_i, %alarm_item) 	= @_;
+
+	my $return_value_default 	= 1;
+	my $return_value_onresult 	= 0;
+
+
+	if($e_or_i eq "i")
+	{
+		#if nothing is found, then continue to the excluded checks
+		$return_value_default 	= 1;
+		$return_value_onresult 	= 0;
+	}
+	elsif($e_or_i eq "e")
+	{
+		#if nothing is found, then continue to the output of the alarm
+		$return_value_default 	= 1;
+	}
+
+
+
+
+	#reset hash by reading elements from hash, else the while would be skipped
+	scalar keys %checks;
+
+	while((my $item) = each %checks)
+	{
+		#Check if object is excluded
+		if($checks{$item}{'MODE'} eq $e_or_i && $checks{$item}{'SELECT'} eq "object")
+		{
+			my $first_char = substr($checks{$item}{'SUBSELECT'}, 0, 1);
+			my $last_char 	= substr($checks{$item}{'SUBSELECT'}, -1);
+
+			#if wildcard syntax is used
+			if($first_char eq "*" && $last_char eq "*")
+			{
+
+				my $tmp1 = substr($checks{$item}{'SUBSELECT'}, 1);
+				my $tmp2 = substr($tmp1, 0,-1);
+
+				#check if the alarm contains the check option 
+				if(index($alarm_item{'OBJECT'}, $tmp2) != -1)
+				{
+					return $return_value_onresult;
+				}
+			}
+			else
+			{
+				#else check for and exact match
+				if($checks{$item}{'SUBSELECT'} eq $alarm_item{'OBJECT'})
+				{
+					return $return_value_onresult;
+				}
+			}
+
+			
+		}
+		#Check if status warning or critical is excluded
+		elsif($checks{$item}{'MODE'} eq $e_or_i && $checks{$item}{'SELECT'} eq "status")
+		{
+
+			if($checks{$item}{'SUBSELECT'} eq "warning" && $alarm_item{'STATUS'} eq "yellow")
+			{
+				return $return_value_onresult;
+			}
+			elsif($checks{$item}{'SUBSELECT'} eq "critical" && $alarm_item{'STATUS'} eq "red")
+			{
+				return $return_value_onresult;
+			}
+		}
+		#Check if type like vm, ds or esx is excluded
+		elsif($checks{$item}{'MODE'} eq $e_or_i && $checks{$item}{'SELECT'} eq "type")
+		{
+			if($checks{$item}{'SUBSELECT'} eq "vm" && $alarm_item{'TYPE'} eq "VirtualMachine")
+			{
+				return $return_value_onresult;
+			}
+			elsif($checks{$item}{'SUBSELECT'} eq "ds" && $alarm_item{'TYPE'} eq "Datastore")
+			{
+				return $return_value_onresult;
+			}
+			elsif($checks{$item}{'SUBSELECT'} eq "esx" && $alarm_item{'TYPE'} eq "HostSystem")
+			{
+				return $return_value_onresult;
+			}
+		}
+		#Check if type like vm, ds or esx is excluded
+		elsif($checks{$item}{'MODE'} eq $e_or_i && $checks{$item}{'SELECT'} eq "datacenter" && $checks{$item}{'SUBSELECT'} eq $alarm_item{'DATACENTER'})
+		{
+			return $return_value_onresult;
+		}
+
+
+	}
+
+	#return true or false depending on the rule match
+	return($return_value_default);
+}
+
+#get all alarms from the vpshere
+sub get_alarms
+{
+	my $alarmMgr         = Vim::get_view(mo_ref => Vim::get_service_content()->alarmManager);
+	my $datacenter_views = Vim::find_entity_views(view_type => "Datacenter");
+
+
+	if(!defined($datacenter_views))
+	{
+		die("Datacenter objects not found\n");
+		exit 3;
+	}
+
+
+	if(defined($option_check))
+	{
+		#split explicit checks to array
+		my @option_check_splitted = split /,/, $option_check;
+
+		my $i = 0;
+		foreach(@option_check_splitted)
+		{
+			#seperate include and exclude from the select
+	    	my @splitted = split /:/, $_;
+
+	    	$checks{$i}{"MODE"} 		= $splitted[0];
+
+	   
+	    	#split the select and subselect
+	    	if(defined($splitted[1]))
+	    	{
+		    	my @splitted_select = split /=/, $splitted[1];
+				foreach(@splitted_select)
+				{
+		    		$checks{$i}{"SELECT"} 		= $splitted_select[0];
+		    		$checks{$i}{"SUBSELECT"} 	= $splitted_select[1];
+		    	}
+	    	}
+	    	else
+	    	{
+	    		print "Value for -C is not correct. Type parameter --help for more information.\n";
+	    		exit 3;
+	    	}
+
+	    	$i++;
+		}
+	}
+
+
+
+	#----------------------------------------------------------------------
+	#Loop through all alarms
+
+	foreach my $datacenter_view (@$datacenter_views)
+	{
+		if(defined($datacenter_view->triggeredAlarmState))
+		{
+			foreach my $alarms (@{$datacenter_view->triggeredAlarmState})
+			{
+				my $entity 					= Vim::get_view(mo_ref => $alarms->entity);
+				my $alarm 					= Vim::get_view(mo_ref => $alarms->alarm);
+				my $check_rule_status_i 	= 0;
+				my $check_rule_status_e 	= 1;
+				my %alarm_item;
+
+				$alarm_item{"NAME"} 		= $alarm->info->name;
+				$alarm_item{"STATUS"} 		= $alarms->overallStatus->val;
+				$alarm_item{"OBJECT"} 		= $entity->name;
+				$alarm_item{"DATACENTER"} 	= $datacenter_view->name;
+				$alarm_item{"TYPE"} 		= $alarms->entity->type;
+
+
+				#only if parameter -C is given
+				if(defined($option_check))
+				{
+					$check_rule_status_i = check_rules("i", %alarm_item);
+				}
+
+
+				#by default just ignore the statement if no included found or -C is not given
+				if($check_rule_status_i == 0)
+				{
+					#only if parameter -C is given
+					if(defined($option_check))
+					{
+						$check_rule_status_e = check_rules("e", %alarm_item);
+					}
+
+					#if no check rules are defined, this will always be true
+					if($check_rule_status_e == 1)
+					{
+
+						if($alarms->overallStatus->val eq "yellow")
+						{
+							$output_nagios_extended .= "Warning - ";
+							$count_warning++;
+						}
+						elsif($alarms->overallStatus->val eq "red")
+						{
+							$output_nagios_extended .= "Critical - ";
+							$count_critical++;
+						}
+						else
+						{
+							$output_nagios_extended .= "Unknown - ";
+						}
+
+						$output_nagios_extended .= $alarm_item{"NAME"}." on ".$alarm_item{"OBJECT"} ." (".$alarm_item{"DATACENTER"}.": ".$alarm_item{"TYPE"}.")\n";
+					}
+				}
+			}
+		}
+	} 
+
+
+	#if any critical alarms found exit with critical code
+	if($count_critical > 0)
+	{
+		$output_return_code = 2;
+	}
+	elsif($count_warning > 0)
+	{
+		$output_return_code = 1;
+	}
+	else
+	{	
+		$output_return_code = 0;
+	}
+
+
+	Util::disconnect();
+}
+
+#summarize alarms and build output string for nagios/icinga return
+sub output_nagios
+{
+	my $count_alarms_all = $count_warning + $count_critical;
+
+	if($output_return_code == 0)
+	{
+		$output_nagios .= "OK - No alarms found";
+	}
+	elsif($output_return_code == 1)
+	{
+		$output_nagios .= "Warning - ".$count_alarms_all." alarms found. View extended output for more information.";
+	}
+	elsif($output_return_code == 2)
+	{
+		$output_nagios .= "Critical - ".$count_alarms_all." alarms found. View extended output for more information.";
+	}
+	else
+	{
+		$output_nagios .= "Unknown - No Status available";
+	}
+
+	print $output_nagios."\n".$output_nagios_extended;
+}
+
+
+#---------------------------------------------------------------------------------------
+# Call functions
+
+get_options();
+connect_vsphere();
+get_alarms();
+output_nagios();
+
+#exit with the appropriate code for nagios
+exit $output_return_code;
